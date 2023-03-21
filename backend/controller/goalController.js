2a7b0a2d86726771df14882938e874601ed6b915
Victor Oluwatobi Akande <akandevictor846@gmail.com>
2023-03-20 13:09:32 -1100

    connected to mongoDb

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,22 +1,29 @@
 const asyncHandler = require('express-async-handler')
+const Goal = require('../model/goalModel') 
 //@desc Get goals
 //@route GET /api/goals
 //@access Private
 const getGoals = asyncHandler(async(req,res)=>{
-    res.status(200).json({message:"Get Goals"});
+    const goals = await Goal.find();  
+    res.status(200).json(goals);
 
 });
 
 
 //@desc Set goal
 //@route POST /api/goals
-//@access Private
+//@access Private 
 const setGoal = asyncHandler (async(req,res)=>{
     if (!req.body.text) {
         res.status(400);
         throw new Error('Please add a text field')
     }
-    res.status(200).json({message:"Set Goals"});
+
+    const goal = await Goal.create({
+        text: req.body.text
+    });
+
+    res.status(200).json({message:"Goal Created Successfully", data: goal});
 
 })
 
@@ -25,7 +32,17 @@ const setGoal = asyncHandler (async(req,res)=>{
 //@route PUT /api/goals/:id
 //@access Private
 const updateGoal = asyncHandler( async(req,res)=>{
-    res.status(200).json({message:`Update Goals ${req.params.id}`});
+
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400);
+        throw new error ('Goal not found');
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true});
+
+    res.status(200).json(updatedGoal);
 })
 
 
@@ -34,7 +51,16 @@ const updateGoal = asyncHandler( async(req,res)=>{
 //@route DELETE /api/goals/:id
 //@access Private
 const deleteGoal = asyncHandler( async (req,res)=>{
-    res.status(200).json({message:`Delete Goals ${req.params.id}`});
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400);
+        throw new error ('Goal not found');
+    }
+
+    const deletedGoal = await Goal.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({message:`Goal deleted sucessfully`, data: deletedGoal});
 })
 
 module.exports = {
